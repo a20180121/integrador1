92cd777e6068d2d91362bafd1e724982ace28f48
a20180121 <131292780+a20180121@users.noreply.github.com>
2023-09-13 12:42:53 -0500

    Add files via upload

---
+++ integrador.js
@@ -0,0 +1,15 @@
+alert ("Bienvebidos al Hakka")
+dinero=parseInt(prompt("¿Cuanto dinero traes?"))
+console.log("trajo: ",dinero)
+entrada=parseInt(prompt("Ingrse el valor de la entrada"))
+console.log("El valor de la entrada es: ",entrada)
+segundo=parseInt(prompt("Ingrse el valor de la del plato de fondo"))
+console.log("El valor de la del segundo es: ",segundo)
+postre=parseInt(prompt("Ingrse el valor de la del podtre"))
+console.log("El valor de la del postre es: ",postre)
+total=entrada+segundo+postre
+console.log("El valor de la del total es: ",total)
+igv=total+18/100
+console.log("El valor co el IGV es: ",igv+total)
+vuelto=dinero-total
+console.log("tu vuelto seria: ",vuelto)
